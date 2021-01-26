12ba3127a5c50a9f3179d2b6baf1aeaa0b20167c
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-01-26 10:16:23 -0500

    update app.js
    updated more comments

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,7 +1,7 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-
+// Variables used in app.js file
 let game;
 const startBtn = document.getElementById('btn__reset');
 const overlay = document.getElementById('overlay');
